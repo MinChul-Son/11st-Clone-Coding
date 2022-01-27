5eccd27a34e4c012887777ea22ac0774c077579f
MinChul-Son <smc5236@naver.com>
2022-01-27 17:06:09 +0900

    🚀 feat: 플러그인 App.vue에 적용

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@ import App from '~/App.vue'
 import router from '~/routes'
 import store from '~/store'
 import fetch from '~/plugins/fetch'
+import lazyLoad from '~/plugins/lazyLoad'
+import search from '~/plugins/search'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
 app.use(fetch)
+app.use(lazyLoad)
+app.use(search)
 app.mount('#app')
