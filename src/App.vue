414d19b05772a2e9a450e6ab14fc61c9def0eb4b
MinChul-Son <smc5236@naver.com>
2022-01-27 19:12:19 +0900

    ✨ feat: 컴포넌트들을 앱에 연결

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,64 @@
 <template>
   <LNB />
+  <RNB />
   <TheHeader />
+  <Billboard />
+  <Direct />
+  <HotFocus />
+  <TimeDeal />
+  <Airlift />
+  <PlanBanner />
+  <WideBanner />
+  <BestProduct />
+  <TodaysChance />
+  <Promotion />
+  <MdsRecommendation />
+  <Partner />
+  <TheFooter />
+  <GlobalBanner />
 </template>
 
 <script>
 import LNB from '~/components/LNB'
+import RNB from '~/components/RNB'
 import TheHeader from '~/components/TheHeader'
-
+import Billboard from '~/components/Billboard'
+import Direct from '~/components/Direct'
+import HotFocus from '~/components/HotFocus'
+import TimeDeal from '~/components/TimeDeal'
+import Airlift from '~/components/Airlift'
+import PlanBanner from '~/components/PlanBanner'
+import WideBanner from '~/components/WideBanner'
+import BestProduct from '~/components/BestProduct'
+import TodaysChance from '~/components/TodaysChance'
+import Promotion from '~/components/Promotion'
+import MdsRecommendation from '~/components/MdsRecommendation'
+import Partner from '~/components/Partner'
+import TheFooter from '~/components/TheFooter'
+import GlobalBanner from '~/components/GlobalBanner'
 export default {
   components: {
     LNB,
-    TheHeader
+    RNB,
+    TheHeader,
+    Billboard,
+    Direct,
+    HotFocus,
+    TimeDeal,
+    Airlift,
+    PlanBanner,
+    WideBanner,
+    BestProduct,
+    TodaysChance,
+    Promotion,
+    MdsRecommendation,
+    Partner,
+    TheFooter,
+    GlobalBanner
   }
 }
 </script>
+
 
 <style lang="scss">
 @import "~/scss/main";
